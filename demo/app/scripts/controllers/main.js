f0f69acfec816f45553ff8551f06212bcd300979
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-04-16 07:45:09 +0530

    Adding demo app

---
+++ demo/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngSuduloonuTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
